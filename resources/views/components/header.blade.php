8006ed4b5044441c45337df315cabbeb9fe3cc36
Nurlan270 <nurlanismzade@gmail.com>
2024-10-05 10:39:29 +0400

    header image alt text changed

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header class="flex items-center justify-center h-14 bg-white border-b-2 border-b-gray-100 overflow-hidden">
     <a href="/" class="flex items-center ">
-        <img class="h-auto w-44" src="{{ asset('assets/logo.png') }}" alt="Pinterest logo">
+        <img class="h-auto w-44" src="{{ asset('assets/logo.png') }}" alt="Pinterest">
     </a>
 </header>
