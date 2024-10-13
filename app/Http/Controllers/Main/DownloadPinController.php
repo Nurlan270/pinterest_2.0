0a571a787535c265b03c4e696a3322f31e27d5da
Nurlan270 <nurlanismzade@gmail.com>
2024-10-13 18:31:46 +0400

    Pin download test

--- app/Http/Controllers/Main/DownloadPinController.php
+++ app/Http/Controllers/Main/DownloadPinController.php
@@ -11,8 +11,7 @@ class DownloadPinController extends Controller
 {
     public function __invoke($pin)
     {
-        dd($pin, Storage::download('pins/'.$pin)->isEmpty());
-        return Storage::exists('pins/'.$pin)
+        return ! Storage::download('pins/'.$pin)->isEmpty()
             ? Storage::download('pins/'.$pin, \request()->input('name'))->send()
             : back();
     }
