29c695f5eb51e66f70dbb42a39360faa3dd86254
Nurlan270 <nurlanismzade@gmail.com>
2024-10-14 16:21:18 +0400

    Account deletion bug fixed + added more detailed deletion specs

--- app/Http/Controllers/Dashboard/DeleteAccountController.php
+++ app/Http/Controllers/Dashboard/DeleteAccountController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pin;
+use App\Models\Saves;
+use App\Models\Subscribers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,7 +13,18 @@ class DeleteAccountController extends Controller
 {
     public function __invoke(): \Illuminate\Http\RedirectResponse
     {
+        // Delete User
         User::query()->where('id', auth()->id())->delete();
+
+        // Delete all related data
+        Pin::query()->where('user_id', auth()->id())->delete();
+
+        Saves::query()->where('user_id', auth()->id())->delete();
+
+        Subscribers::query()
+            ->where('author_id', auth()->id())
+            ->orWhere('subscriber_id', auth()->id())
+            ->delete();
 
         auth()->logout();
 
