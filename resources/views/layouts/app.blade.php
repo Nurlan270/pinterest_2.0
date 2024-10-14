7ea300ead4599ed0cdf254df209e1636b6674851
Nurlan270 <nurlanismzade@gmail.com>
2024-10-14 19:06:25 +0400

    Added SEO Meta Tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+{{--  Meta tags  --}}
+    <meta name="description" content="Explore, discover, and share creative ideas on Pinterest, save your favorite inspirations, and interests. Join our community today!">
+    <meta name="keywords" content="Pinterest clone, save ideas, discover creativity, image sharing, social media, creative projects, inspiration, DIY, home decor, fashion, recipes">
+
+    <meta property="og:title" content="Pinterest - Discover, Save, and Share Creative Ideas">
+    <meta property="og:description" content="Pinterest is a platform where you can find, save, and share creative ideas, projects, and inspirations.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ config('app.url') }}">
+    <meta property="og:image" content="{{ asset('assets/minimized-logo.png') }}">
+
+    <meta name="robots" content="index, follow">
+
     <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon">
     <title>
         @if(Route::is('home'))
